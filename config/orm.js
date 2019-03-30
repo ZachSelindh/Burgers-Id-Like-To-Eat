faaf7583bd2995ada63a71af41a39a0132e0080b
Zach Selindh <zachselindh@gmail.com>
2019-03-30 17:52:01 -0500

    Burgers can now be eaten.

--- config/orm.js
+++ config/orm.js
@@ -10,19 +10,26 @@ function printQuestionMarks(num) {
   return arr.toString();
 }
 
-// Helper function to convert values to MySQL-readable.
+// Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
   var arr = [];
+
   // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
+    // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
   }
+
+  // translate array of strings to a single comma-separated string
   return arr.toString();
 }
 
@@ -45,9 +52,11 @@ var orm = {
     });
   },
   updateOne: function(table, objColVals, condition, cb) {
+    console.log("Obj: " + objToSql(objColVals));
     let queryString = "UPDATE " + table + " SET ";
     queryString += objToSql(objColVals) + " WHERE ";
     queryString += condition;
+    console.log("Query at orm: " + queryString);
     connection.query(queryString, function(err, data) {
       if (err) throw err;
       cb(data);
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,5 +13,23 @@ router.get("/", function(req, res) {
   });
 });
 
+router.put("/api/burgers/:id", function(req, res) {
+  let condition = "id = " + req.params.id;
+  console.log("condition at cont:" + condition);
+  Burger.update(
+    {
+      devoured: 1
+    },
+    condition,
+    function(data) {
+      if (data.changedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    }
+  );
+});
+
 module.exports = router;
 // router is exported to server.js

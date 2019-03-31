59720fb7809d766860948009c812f4a9718583f0
Zach Selindh <zachselindh@gmail.com>
2019-03-30 20:56:35 -0500

    Added JAWSDB connection for Heroku.

---
+++ database/connectJAWSDB.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+burger_name VARCHAR(100),
+devoured BOOLEAN DEFAULT FALSE
+);
+
+INSERT INTO burgers (burger_name)
+VALUES 
+("Double-Double"),
+("Bacon Whataburger"),
+("Sonic Jalapeno Double")
+ ;
